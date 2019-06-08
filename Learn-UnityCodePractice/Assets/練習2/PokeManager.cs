14521275d3ed7c795ec81f7384c85591743bd5cc
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-08 08:15:12 +0800

    6/8 更新

--- Learn-UnityCodePractice/Assets/練習2/PokeManager.cs
+++ Learn-UnityCodePractice/Assets/練習2/PokeManager.cs
@@ -8,12 +8,13 @@ public class PokeManager : MonoBehaviour
     /// <summary>
     /// 名稱 等級 經驗 攻擊力 防禦力 血量/血量
     /// </summary>
-    public Pokemon firstPet = new Pokemon("小火龍",10,10,4,5,15,15);
+    public Pokemon firstPet = new Pokemon("小火龍",3,10,4,5,15,15);
     #endregion
     private void Start()
     {
         //呼叫lvch方法顯示基本屬性
         firstPet.lvch();
+        
     }
 
     
